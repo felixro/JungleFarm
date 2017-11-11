c9c583ee90ea4643863b9e3f2e4962b0ca5894b4
Felix Rohrer <Felix.Rohrer@Sophos.com>
2017-11-11 10:00:16 -0500

    Created proper 2d borders.

---
+++ Assets/scripts/MonkeyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonkeyController : MonoBehaviour 
+{
+    public BorderController border;
+
+    public GameObject prefab;
+
+    public float borderOffset = 0.5f;
+    public int numberOfPrefabs = 10;
+
+	void Start() 
+    {
+        for (int i=0;i<numberOfPrefabs;i++)
+        {
+            Instantiate();
+        }
+	}
+
+    void Instantiate()
+    {
+        Vector3 position = new Vector3(
+            Random.Range(
+                transform.position.x - border.bounds.extents.x + borderOffset, 
+                transform.position.x + border.bounds.extents.x - borderOffset
+            ), 
+            Random.Range(
+                transform.position.y - border.bounds.extents.y + borderOffset, 
+                transform.position.y + border.bounds.extents.y - borderOffset
+            ), 
+            0
+        );
+
+        Instantiate(prefab, position, Quaternion.identity);
+    }
+}
